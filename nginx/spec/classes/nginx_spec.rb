964cc8febdd31d2f78b4042d4cd512d80450ae71
li23 <chisholmtw@yahoo.com>
2024-01-23 12:57:15 -0800

    Update nginx_spec.rb

--- nginx/spec/classes/nginx_spec.rb
+++ nginx/spec/classes/nginx_spec.rb
@@ -6,8 +6,10 @@ describe 'nginx' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
+      
       it { is_expected.to compile }
       it { is_expected.to contain_service('nginx').that_requires('Package[nginx]') }
     end
   end
 end
+
